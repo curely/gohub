d5b59a43cea7c3d5bd22362016628889d9b6234d
deven <zqk_cf@163.com>
2022-01-17 18:03:25 +0800

    配置方案实现

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// new一个Gin Engine实例
 	router := gin.New()
 
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,10 @@
 package config
 
 import (
+	"github.com/spf13/cast"
 	viperlib "github.com/spf13/viper" // 自定义包名，避免与内置viper实例冲突
+	"gohub/pkg/helpers"
+	"os"
 )
 
 // viper 库实例
@@ -12,20 +15,20 @@ var viper *viperlib.Viper
 type ConfigFunc func() map[string]interface{}
 
 // ConfigFuncs 先加载到此数组，loadConfig 在动态生成配置信息
-var ConfigFuncs map[string]interface{}
+var ConfigFuncs map[string]ConfigFunc
 
 func init() {
 
-	// 1. 初始化Viper库
+	// 1. 初始化 Viper 库
 	viper = viperlib.New()
 	// 2. 配置类型，支持 "json", "toml", "yaml", "yml", "properties",
-	//                "props", "prop", "env", "dotenv"
+	//             "props", "prop", "env", "dotenv"
 	viper.SetConfigType("env")
-	// 3. 环境变量配置文件查找的路径，相对于main.go
+	// 3. 环境变量配置文件查找的路径，相对于 main.go
 	viper.AddConfigPath(".")
-	// 4. 设置环境变量前缀，用以区分Go的系统环境变量
+	// 4. 设置环境变量前缀，用以区分 Go 的系统环境变量
 	viper.SetEnvPrefix("appenv")
-	// 5. 读取环境变量（支持flags）
+	// 5. 读取环境变量（支持 flags）
 	viper.AutomaticEnv()
 
 	ConfigFuncs = make(map[string]ConfigFunc)
@@ -37,4 +40,105 @@ func InitConfig(env string) {
 	loadEnv(env)
 	// 2. 注册配置信息
 	loadConfig()
+}
+
+func loadConfig() {
+	for name, fn := range ConfigFuncs {
+		viper.Set(name, fn())
+	}
+}
+
+func loadEnv(envSuffix string) {
+
+	// 默认加载 .env 文件，如果有传参 --env=name的话，加载.env.name 文件
+	envPath := ".env"
+	if len(envSuffix) > 0 {
+		filepath := ".env." + envSuffix
+		if _, err := os.Stat(filepath); err == nil {
+			// 如 .env.testing 或 .env.stage
+			envPath = filepath
+		}
+	}
+
+	// 加载env
+	viper.SetConfigName(envPath)
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	// 监控 .env 文件，变更是重新加载
+	viper.WatchConfig()
+}
+
+// Env 读取环境变量，支持默认值
+func Env(envName string, defaultValue ...interface{}) interface{} {
+	if len(defaultValue) > 0 {
+		return internalGet(envName, defaultValue[0])
+	}
+	return internalGet(envName)
+}
+
+// Add 新增配置项
+func Add(name string, configFn ConfigFunc) {
+	ConfigFuncs[name] = configFn
+}
+
+// Get 获取配置项
+// 第一个参数 path 允许使用点式获取，如：app.name
+// 第二个参数允许传默认值
+func Get(path string, defaultValue ...interface{}) string {
+	return GetString(path, defaultValue...)
+}
+
+func internalGet(path string, defaultValue ...interface{}) interface{} {
+	// config或者环境变量不存在的情况
+	if !viper.IsSet(path) || helpers.Empty(viper.Get(path)) {
+		if len(defaultValue) > 0 {
+			return defaultValue[0]
+		}
+
+		return nil
+	}
+
+	return viper.Get(path)
+}
+
+// GetString 获取 String 类型的配置信息
+func GetString(path string, defaultValue ...interface{}) string {
+	return cast.ToString(internalGet(path, defaultValue...))
+}
+
+// GetInt 获取Int类型的配置信息
+func GetInt(path string, defaultValue ...interface{}) int {
+	return cast.ToInt(internalGet(path, defaultValue...))
+}
+
+// GetFloat64 获取float64类型的配置信息
+func GetFloat64(path string, defaultValue ...interface{}) float64 {
+	return cast.ToFloat64(internalGet(path, defaultValue...))
+}
+
+// GetInt64 获取Int64类型的配置信息
+func GetInt64(path string, defaultValue ...interface{}) int64 {
+	return cast.ToInt64(internalGet(path, defaultValue...))
+}
+
+// GetUint 获取Uint类型的配置信息
+func GetUint(path string, defaultValue ...interface{}) uint {
+	return cast.ToUint(internalGet(path, defaultValue...))
+}
+
+// GetBool 获取Bool类型的配置信息
+func GetBool(path string, defaultValue ...interface{}) bool {
+	return cast.ToBool(internalGet(path, defaultValue...))
+}
+
+// GetStringMapString 获取数据结构
+func GetStringMapString(path string) map[string]string {
+	return viper.GetStringMapString(path)
+}
+
+// Initiallize 触发加载 config 包的所有 init 函数
+func Initiallize() {
+
 }
